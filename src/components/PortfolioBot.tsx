e426a9e1c8ae57842a8ab93f35ee51269eb78914
eccreativ <chase@eccreativ.com>
2026-02-11 15:25:37 -0600

    [eyeglass:eyeglass-1770845126549-7wedhoo] Scroll fixed!

--- src/components/PortfolioBot.tsx
+++ src/components/PortfolioBot.tsx
@@ -160,7 +160,7 @@ export default function PortfolioBot() {
         </div>
 
         {/* Messages */}
-        <div className="flex-1 overflow-y-auto p-4 space-y-3">
+        <div className="flex-1 overflow-y-auto overscroll-contain p-4 space-y-3" style={{ WebkitOverflowScrolling: 'touch' }}>
           {messages.map((message, index) => (
             <div
               key={index}
